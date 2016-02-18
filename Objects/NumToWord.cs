3b1153d55ebbb927a1554e8a32a7d88a3a450198
Nicholas Jensen and Michael Smith <student@epicodus.com>
2016-02-18 15:52:12 -0800

    add 20 tests. Make them pass

--- Objects/NumToWord.cs
+++ Objects/NumToWord.cs
@@ -1,19 +1,76 @@
 using System.Collections.Generic;
 using System;
+using System.Text.RegularExpressions;
 namespace NumToWordNS.Objects
 {
   public class NumToWord
   {
-    private int _num;
+    private long _num;
+    private string[] _upToTwenty;
+    private string[] _tensPlace;
 
-    public NumToWord(int num)
+    public NumToWord(long num)
     {
       _num = num;
+      _upToTwenty = new string[21] {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen", "twenty" };
+      _tensPlace = new string[8] {"twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
     }
 
+    private List<string> _output;
     public string ChangeToWord()
     {
-      return "one";
+      long num = _num;
+      _output = new List<string>(){};
+      Dictionary<string,long> placesToCheck = new Dictionary<string,long>(){};
+      placesToCheck.Add("quadrillion", 1000000000000000);
+      placesToCheck.Add("trillion",    1000000000000);
+      placesToCheck.Add("billion",     1000000000);
+      placesToCheck.Add("million",     1000000);
+      placesToCheck.Add("thousand",    1000);
+      foreach(var item in placesToCheck)
+      {
+        num = AboveThousand(num, item.Value, item.Key);
+      }
+      if(num > 0 && num < 1000)
+        _output.Add(LessThanThousand(num));
+      return string.Join(" ", _output.ToArray());
     }
+    public long AboveThousand(long num, long place, string name)
+    {
+      if( num >= place )
+      {
+        _output.Add(LessThanThousand((num - num % place) / place) + " " + name);
+        num = num - (num - (num % place));
+        // Console.WriteLine("Millions, Num Value: " + num);
+      }
+      return num;
+    }
+
+    public string LessThanThousand(long num)
+    {
+      List<string> output = new List<string>(){};
+      if( num >= 100 && num < 1000)
+      {
+        long hundreds = (num - (num % 100)) / 100; //has to equal num + 00
+        output.Add(_upToTwenty[hundreds] + " hundred");
+        num = num - (hundreds * 100);
+        // Console.WriteLine("Hundreds, Num Value: " + num);
+      }
+      if( num > 20 && num < 100 )
+      {
+        long tens = (num - (num % 10)) / 10 - 2;
+        output.Add(_tensPlace[tens]);
+        num = num - (num - (num % 10));
+        // Console.WriteLine("Tens, Num Value: " + num);
+      }
+      if(num > 0)
+      {
+        output.Add(_upToTwenty[num]);
+      }
+
+      // Console.WriteLine(string.Join(" ", output.ToArray()).Trim());
+     return string.Join(" ", output.ToArray()).Trim();
+    }
+
   } // end class
 } // end namespace
--- Tests/NumToWordTest.cs
+++ Tests/NumToWordTest.cs
@@ -10,32 +10,147 @@ namespace NumToWordNS
     public void Test_For_1()
     {
      NumToWord translation = new NumToWord(1);
-
      Assert.Equal("one", translation.ChangeToWord());
-
     }
     // turns 10 to ten
-
+    [Fact]
+    public void Test_For_10()
+    {
+     NumToWord translation = new NumToWord(10);
+     Assert.Equal("ten", translation.ChangeToWord());
+    }
     // turns 19 to nineteen
-
+    [Fact]
+    public void Test_For_19()
+    {
+     NumToWord translation = new NumToWord(19);
+     Assert.Equal("nineteen", translation.ChangeToWord());
+    }
     // turns 20 to twenty
-
+    [Fact]
+    public void Test_For_20()
+    {
+     NumToWord translation = new NumToWord(20);
+     Assert.Equal("twenty", translation.ChangeToWord());
+    }
     // turns 21 to twenty one
-
+    [Fact]
+    public void Test_For_21()
+    {
+     NumToWord translation = new NumToWord(21);
+     Assert.Equal("twenty one", translation.ChangeToWord());
+    }
     // turns 55 to fifty five
-
+    [Fact]
+    public void Test_For_55()
+    {
+     NumToWord translation = new NumToWord(55);
+     Assert.Equal("fifty five", translation.ChangeToWord());
+    }
     // turns 100 to one hundred
+    [Fact]
+    public void Test_For_100()
+    {
+     NumToWord translation = new NumToWord(100);
+     Assert.Equal("one hundred", translation.ChangeToWord());
+    }
     // turns 101 to one hundred and one
+    [Fact]
+    public void Test_For_101()
+    {
+     NumToWord translation = new NumToWord(101);
+     Assert.Equal("one hundred one", translation.ChangeToWord());
+    }
     // turns 110 to one hundred and ten
+    [Fact]
+    public void Test_For_110()
+    {
+     NumToWord translation = new NumToWord(110);
+     Assert.Equal("one hundred ten", translation.ChangeToWord());
+    }
     // turns 158 to one hundred and fifty eight
+    [Fact]
+    public void Test_For_158()
+    {
+     NumToWord translation = new NumToWord(158);
+     Assert.Equal("one hundred fifty eight", translation.ChangeToWord());
+    }
     // turns 1000 into one thousand
+    [Fact]
+    public void Test_For_1000()
+    {
+     NumToWord translation = new NumToWord(1000);
+     Assert.Equal("one thousand", translation.ChangeToWord());
+    }
     // turns 1639 to one thousand six hundred thirty nine
+    [Fact]
+    public void Test_For_1639()
+    {
+     NumToWord translation = new NumToWord(1639);
+     Assert.Equal("one thousand six hundred thirty nine", translation.ChangeToWord());
+    }
     // turns 10,000 to ten thousand
-    // turns 100000 to one hundred thousand
+    [Fact]
+    public void Test_For_10000()
+    {
+     NumToWord translation = new NumToWord(10000);
+     Assert.Equal("ten thousand", translation.ChangeToWord());
+    }
+    // turns 100,000 to one hundred thousand
+    [Fact]
+    public void Test_For_100000()
+    {
+     NumToWord translation = new NumToWord(100000);
+     Assert.Equal("one hundred thousand", translation.ChangeToWord());
+    }
     // turns 1,000,000 to one million
+    [Fact]
+    public void Test_For_1000000()
+    {
+     NumToWord translation = new NumToWord(1000000);
+     Assert.Equal("one million", translation.ChangeToWord());
+    }
     // turns 1,000,000,000 to one billion
+    [Fact]
+    public void Test_For_1000000000()
+    {
+     NumToWord translation = new NumToWord(1000000000);
+     Assert.Equal("one billion", translation.ChangeToWord());
+    }
     // turns 1,000,000,000,000 to one trillion
+    [Fact]
+    public void Test_For_1000000000000()
+    {
+     NumToWord translation = new NumToWord(1000000000000);
+     Assert.Equal("one trillion", translation.ChangeToWord());
+    }
     // turns 5,937,209,532,109 to five trillion nine hundred thirty seven billion two hundred and nine million five hundred thirty two one hundred and nine
+    [Fact]
+    public void Test_For_5937209532109()
+    {
+     NumToWord translation = new NumToWord(5937209532109);
+     Assert.Equal("five trillion nine hundred thirty seven billion two hundred nine million five hundred thirty two thousand one hundred nine", translation.ChangeToWord());
+    }
+    // turns 999,999,999,999,999 into nine hundred ninety nine trillion nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine
+    [Fact]
+    public void Test_For_999999999999999()
+    {
+     NumToWord translation = new NumToWord(999999999999999);
+     Assert.Equal("nine hundred ninety nine trillion nine hundred ninety nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred ninety nine", translation.ChangeToWord());
+    }
+
+    // WORKS FOR ONE QUADRILLION
+    [Fact]
+    public void Test_for_quadrillion()
+    {
+      Assert.Equal("one quadrillion", new NumToWord(1000000000000000).ChangeToWord());
+    }
+    // WORKS FOR ONE QUADRILLION
+    [Fact]
+    public void Test_for_100quadrillion()
+    {
+      Assert.Equal("one hundred quadrillion", new NumToWord(100000000000000000).ChangeToWord());
+    }
 
     /* EXAMPLE
     // Have a queen object that knows what coordinants its at
